f100c2f42fc8e4ef92ecaec0ed0bc5e87a8f94f6
d3m1s4m <d3m1s4m@gmail.com>
2024-10-11 15:15:00 +0330

    Fix command-line arguments in main.py to be required

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from db import create_db_engine
 
 # Command-line argument parsing
 parser = argparse.ArgumentParser(description='Database configuration')
-parser.add_argument('-H', '--host', help='Database host')
-parser.add_argument('-U', '--user', help='Database username')
-parser.add_argument('-P', '--password', help='Database password')
-parser.add_argument('-D', '--dbname', help='Database name')
+parser.add_argument('-H', '--host', required=True, help='Database host')
+parser.add_argument('-U', '--user', required=True, help='Database username')
+parser.add_argument('-P', '--password', required=True, help='Database password')
+parser.add_argument('-D', '--dbname', required=True, help='Database name')
 
 args = parser.parse_args()
 
