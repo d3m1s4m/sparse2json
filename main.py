087b5463b68d3f5d495a58c432d7c3a029b03d60
d3m1s4m <d3m1s4m@gmail.com>
2024-10-11 15:12:29 +0330

    Update main.py for command-line argument handling

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
+import argparse
+from db import create_db_engine
+
+
+# Command-line argument parsing
+parser = argparse.ArgumentParser(description='Database configuration')
+parser.add_argument('-H', '--host', help='Database host')
+parser.add_argument('-U', '--user', help='Database username')
+parser.add_argument('-P', '--password', help='Database password')
+parser.add_argument('-D', '--dbname', help='Database name')
+
+args = parser.parse_args()
+
+
+# Create the database engine
+engine = create_db_engine(args.host, args.user, args.password, args.dbname)
 
 
 if __name__ == '__main__':
