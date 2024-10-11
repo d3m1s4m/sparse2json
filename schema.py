444ba38fb8b355b52a1cc8f174195911bb5e0bee
d3m1s4m <d3m1s4m@gmail.com>
2024-10-11 21:08:21 +0330

    Update main.py to receive verbosity as argument

--- schema.py
+++ schema.py
@@ -2,6 +2,21 @@ from sqlalchemy import inspect
 from geoalchemy2 import Geometry
 
 
+def get_columns_names(engine, table):
+    inspector = inspect(engine)
+
+    columns = inspector.get_columns(table)
+    columns_name = []
+    for column in columns:
+        # Check for geometry type and handle it
+        if column['type'] == Geometry:
+            columns_name.append(column['name'])
+        else:
+            columns_name.append(column["name"])
+
+    return columns_name
+
+
 def get_tables_and_columns(engine):
     """Get a dictionary of all tables and their columns in the database."""
     inspector = inspect(engine)
@@ -9,15 +24,7 @@ def get_tables_and_columns(engine):
 
     tables = inspector.get_table_names()
     for table in tables:
-        columns = inspector.get_columns(table)
-        columns_name = []
-        for column in columns:
-            # Check for geometry type and handle it
-            if column['type'] == Geometry:
-                columns_name.append(column['name'])
-            else:
-                columns_name.append(column["name"])
-
-        db_info[table] = columns_name
+        db_info[table] = get_columns_names(engine, table)
 
     return db_info
+
