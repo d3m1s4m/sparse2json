3d5962d0f8b8ba79e38c684c81c4503000c91e07
d3m1s4m <d3m1s4m@gmail.com>
2024-10-17 00:48:50 +0330

    Implement multithreading for sparse column conversion process

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import logging.config
+import queue
+import threading
 from cli import parse_arguments, get_db_password, get_min_rows_for_sparse_check, get_sparse_percent
 from config import LOGGING_CONFIG
 from db import create_db_engine
@@ -9,9 +11,28 @@ from sparse_convertor import convert_sparse_to_json
 
 # Configure logging
 logging.config.dictConfig(LOGGING_CONFIG)
+logger = logging.getLogger('sparse2json_logger')
 
 # Parse arguments
 args = parse_arguments()
+
+
+def worker(q, engine, is_verbose):
+    while True:
+        table_to_fix = q.get()
+
+        # Break the loop when None is received
+        if table_to_fix is None:
+            logger.info("Worker received sentinel and is terminating.")
+            q.task_done()
+            break
+
+        try:
+            convert_sparse_to_json(engine, table_to_fix, is_verbose)
+        except Exception as e:
+            logger.error(f'Error while fixing table: {e}')
+        finally:
+            q.task_done()
 
 
 def run():
@@ -37,11 +58,47 @@ def run():
     if args.table:
         tables_to_fix = find_sparse_columns(engine, args.table, min_rows_for_sparse_check, sparse_percent)
         print(tables_to_fix)
+
+        # Convert sparse columns to JSON for the identified tables
+        convert_sparse_to_json(engine, tables_to_fix, is_verbose=args.verbose)
     else:
         tables_to_fix = find_sparse_tables(engine, min_rows_for_sparse_check, sparse_percent)
 
-    # Convert sparse columns to JSON for the identified tables
-    convert_sparse_to_json(engine, tables_to_fix, is_verbose=args.verbose)
+        tables_to_fix_count = len(tables_to_fix)
+
+        # Check if there is more than 1 table to fix to use threading
+        if tables_to_fix_count > 1:
+            # Create task queue
+            q = queue.Queue()
+
+            max_threads = 5
+            thread_count = min(tables_to_fix_count, max_threads)
+
+            # Create worker threads
+            threads = []
+            for i in range(thread_count):
+                t = threading.Thread(target=worker, args=(q, engine, args.verbose))
+                threads.append(t)
+                t.start()
+
+            # Add tasks to queue
+            for table, columns in tables_to_fix.items():
+                q.put({table: columns})
+
+            # Add sentinel (None) for each thread to stop them after task completion
+            for _ in threads:
+                q.put(None)
+
+            # Wait for all tasks to be done
+            q.join()
+
+            # Ensure all threads terminate
+            for t in threads:
+                t.join()
+
+        else:
+            # Convert sparse columns to JSON for the identified tables
+            convert_sparse_to_json(engine, tables_to_fix, is_verbose=args.verbose)
 
     # Check if any tables need fixing
     if not bool(tables_to_fix):
